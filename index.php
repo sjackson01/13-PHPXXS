12424deb1a0da024fab3a08dd09cfcb50c1dc54b
sjackson01 <stevenpjackson0@gmail.com>
2020-03-03 12:23:09 -0600

    Add (index.php> Hunk 1) : Add htmlentities() example.

--- index.php
+++ index.php
@@ -5,8 +5,17 @@
       <title>XSS Attacks</title>
    </head>
    <body>
+   <?php # Script 13.4 - xxs.php
 
-   <form action="xss.php" method="post">
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        
+        // Apply the different function, printing the resutls:
+        echo "<h2>Original</h2><p>{$_POST['data']}</p>";
+        echo '<h2>After htmlentities()</h2><p>' . htmlentities($_POST['data']) . '</p>';        
+        }
+        // Display the form: 
+   ?>
+   <form action="index.php" method="post">
      <p>Do your worst! <textarea name="data" rows="3" cols="40"></textarea></p>
      <div align="center"><input type="submit" name="submit" value="Submit"></div>
     </form>
